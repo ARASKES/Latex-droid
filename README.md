3f4055f162928dce4081efc1ca712a17c59838bb
Andrey Ryzhak <69841157+ARASKES@users.noreply.github.com>
2021-05-07 12:49:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Latex-droid
+ LatexDroid
---
+++ app/src/main/java/com/mirea/bsbo0419/latexdroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mirea.bsbo0419.latexdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
